0c2a904dded101ffa3bb786508764c0ef93a014d
Nataliia Timoshyna <nezhinskaya83@gmail.com>
2021-09-10 14:45:41 +0300

    Update settings.py

--- my_email/settings.py
+++ my_email/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b%^$&vb0^$x8z7m76c#p&g+gmp6-fy6+kyumm*+!02ub2y*9d*'
+SECRET_KEY = '***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,8 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # smtp
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nezhinskaya83@gmail.com'
-EMAIL_HOST_PASSWORD = 'natasha_nezhinsk'
+EMAIL_HOST_USER = 'ivanova@gmail.com'
+EMAIL_HOST_PASSWORD = '***********'
 EMAIL_PORT = 587
 
 #  REDIS related settings
